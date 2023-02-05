70be475fc27f93bcd9f8530e9301dbc5db4d3995
Guilherme Colares <gcolares95@gmail.com>
2023-02-05 16:01:59 -0300

    Criando repositório de categoria

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,33 @@
+import { Category } from "../model/Category";
+
+
+// DTO => Data Transfer Object
+// Podemos usar para transferir dados da nossa rota para o nosso repositório
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  // Método para cadastrar categoria
+  create( {name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date()
+    })
+  
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepository };
