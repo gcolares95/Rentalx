3133ab07c496e4338bf50129364677699a9a0b0a
Guilherme Colares <gcolares95@gmail.com>
2023-03-13 21:37:08 -0300

    Singleton Pattern

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -1,13 +1,23 @@
 import { Category } from "../model/Category";
 import { ICategoriesRepository, ICreateCategoryDTO } from "./ICategoriesRepository";
 
-// "Banco" em memória
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];  // criando tabela "fake" de Specification
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository; // Mesmo tipo da nossa classe
+
+  private constructor() {
     this.categories = [];
   }
+
+  // Método que cria uma instância/repassar uma instância já existente
+  public static getInstace(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository(); // Cria uma instância somente uma vez
+    }
+
+    return CategoriesRepository.INSTANCE;
+  } 
 
   // Método para cadastrar categoria
   create({name, description }: ICreateCategoryDTO): void {
--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -4,8 +4,10 @@ import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 // Instanciar repository e useCase para utilizar nas rotas
 
-const categoryRepository = new CategoriesRepository();
+const categoryRepository = CategoriesRepository.getInstace();
+
 const createCategoryUseCase = new CreateCategoryUseCase(categoryRepository);
+
 const createCategoryController = new CreateCategoryController(createCategoryUseCase);
 
 export { createCategoryController }
